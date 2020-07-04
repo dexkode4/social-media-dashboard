a0d5394c514d50f92a6bc4e28cd7815db9392954
Dexkode <dexkode4@gmail.com>
2020-07-04 21:19:09 +0100

    solution

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
+
 const currentTheme = localStorage.getItem("theme");
 
-// if (currentTheme) {
-//     document.documentElement.setAttribute('data-theme', currentTheme);
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-//     if (currentTheme === 'dark') {
-//         toggleSwitch.checked = true;
-//     }
-// }
-
-function switchTheme(e) {
-  var element = document.body;
-
-  if (e.target.checked) {
-    element.classList.add("light");
-  } else {
-    element.classList.remove("light");
+///// if theme is dark ,on reload let it stays at dark ,as toggleSwitch.checked === true , dark theme will apply
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
   }
-  // if (e.target.checked) {
-  //     document.documentElement.setAttribute('data-theme', 'dark');
-  //     // localStorage.setItem('theme', 'dark');
-  // }
-  // else {        document.documentElement.setAttribute('data-theme', 'light');
-  //     //   localStorage.setItem('theme', 'light');
-  // }
 }
 
-toggleSwitch.addEventListener("change", switchTheme, false);
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme);
